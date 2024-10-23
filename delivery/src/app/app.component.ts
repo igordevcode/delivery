6352c06b4a6a05de5a8ee39c6da2da773138b0d9
igor ribeiro <igordevcode@gmail.com>
2024-10-23 00:03:31 +0000

    INICIAL

---
+++ delivery/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { InicialComponent } from "./inicial/inicial.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, InicialComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'delivery';
+}
